697fe5810d4a58e61299ee8f8fd05d0f2f8d3c1d
Tobias Megies <megies@geophysik.uni-muenchen.de>
2017-07-15 15:18:49 +0200

    temporarily patch constructor, setting makensis verbosity to max

--- patch_makensis_verbosity.py
+++ patch_makensis_verbosity.py
@@ -9,4 +9,10 @@ for line in fileinput.input(file_to_patch, inplace=True):
     if line.strip() == "args = [MAKENSIS_EXE, '/V2', nsi]":
         # increase verbosity to max
         line = line.replace("'/V2'", "'/V4'")
+    if line.strip() == "check_call(args)":
+        # increase verbosity to max
+        line = line.replace(")", ", stdout=STDOUT, stderr=STDOUT)")
+    if line.strip() == "from subprocess import check_call, check_output":
+        # increase verbosity to max
+        line = line.rstrip() + ', STDOUT' + os.linesep
     print(line, end='')
